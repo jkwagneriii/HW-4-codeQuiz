f4c0eea14027c67cefbd8c298299479f61a1e65d
Jake Wagner <jkwagneriii@gmail.com>
2020-09-29 16:44:10 -0500

    initial file push and the start of psuedo coding

---
+++ reference.js
@@ -0,0 +1,29 @@
+// This page is to help supplement my psuedo code page by adding possible reference code to use once I actually start putting it all together.
+
+// Possible countdown timer reference code!
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+<body>
+    <div>Registration closes in <span id="time">05:00</span> minutes!</div>
+</body>
+
